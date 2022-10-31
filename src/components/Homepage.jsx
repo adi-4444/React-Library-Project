89b277532365eef3d0078ac8a84b256e00bb43ad
Adi <adinarayana4444@gmail.com>
2022-11-01 02:08:01 +0530

    React Library Project

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -18,16 +18,15 @@ const Homepage = ({students,tt}) => {
       setTodayDate(date);
       const day = days[new Date().getDay()]
       setCurentDay(day)
-   } 
-   setInterval(refresh,1000)
-
+    } 
+      setInterval(refresh,1000)
 
    // Taking input to state
    const [input, setInput] = useState();
    const submitHandler = (e) => {
       e.preventDefault()
       checkStudent(input,students);
-   }
+    }
 
    //  this function will find student data
    const checkStudent = (input,students) => {
@@ -38,58 +37,98 @@ const Homepage = ({students,tt}) => {
             console.log("Not Found")
          }
          else {
-            // console.log(student);
             checkTimeTable(student,tt);
          }
-   }
+     }
        // check branch and section of student
    const checkTimeTable = (stud,tt) => {
       let filteredBranch = tt.timetables.find(ttb => ttb.branch === stud.branch);
       let filteredSection = filteredBranch.sections.find(ttbs => ttbs.s === stud.s);
       checkDay(filteredSection);
-   }
+     }
       // check present day for section
    const checkDay = (section) => {
       const days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
       let day = days[new Date().getDay()]
+      let cTime = parseFloat(new Date().getHours()+"."+new Date().getMinutes())
       let filteredDay = section.days.find(sdd => sdd.day === day);
-      checkPeriodTime(filteredDay);
-   }
+      if(day === "sunday") {
+         ifSunday(cTime)
+      } else {
+         checkPeriodTime(filteredDay)
+      }
+    }
+      // messages if sunday
+      const ifSunday = (cTime) => {
+         let s = 9.30
+         let e = 18
+         switch (true) {
+            case (cTime >= s && cTime <= e):
+               console.log("You are Allowed")
+               break;
+            case (cTime < s):
+               console.log("Not Allowed : You are early")
+               break;
+            case (cTime > e):
+               console.log("Not Allowed : Library is Closed")
+               break;
+            default:
+               break;
+         }
+       }
+
    // check current time with the period time for day
    const checkPeriodTime =(day) => {
       // let cTime = parseFloat(new Date().getHours()+"."+new Date().getMinutes())
-      let cTime = 10.31
-      console.log(cTime)
+      let cTime = 12
       let periods = day.periods
-      console.log(periods)
+       if(cTime < 9.30) console.log("Not Allowed : You are Early")            
+       if(cTime > 18) console.log("Not Allowed : Library is Closed")
       // this loop will check the current time to period timings and return period
       for (let key in periods) {
          let value = periods[key];
          let start = parseFloat(key.split("-")[0])+0.01 // 0.01 added to overlap 2 classe at a time
          let end = parseFloat(key.split("-")[1])
+
          if(cTime >= start && cTime <= end) {
             console.log(value)
-         }
+            switch (true) {
+               case (value):
+                  console.log("working",value,cTime)
+                  break;
+               case (value === "L"):
+                  console.log(`You are Allowed its a LEISURE Period`)
+                  break;
+               case (value === "LIBRARY"):
+                  console.log(`You are Allowed its ${value} Period`)
+                  break;
+               case (value !== "L" || value !== "LIBRARY"):
+                  console.log(`You are Not Allowed, You have ${value} class now`)
+                  break;
+               default:
+                  break;
+            }
+         }  
        }
-   }
-  return (
-    <div>
-      <h1>Home Page</h1>
-      <span>Date : {todayDate}</span><span> ,  {currentDay}</span>
-      <p> Time : {currentTime}</p>
-      <form onSubmit={submitHandler}>
-        <input type='text' value={input} style={{"textTransform":"uppercase"}} onChange={(e) => setInput(e.target.value)} />
-        <button type='submit'>Submit</button>
-        {
-         // students && students.map(student => (
-         //    <div key={student.id}>
-         //       <p>ID: {student.ID}</p>
-         //    </div>
-         // ))
-        }
-      </form>
-    </div>
-  )
+    }
+      
+   return (
+      <div>
+         <h1>Home Page</h1>
+         <span>Date : {todayDate}</span><span> ,  {currentDay}</span>
+         <p> Time : {currentTime}</p>
+         <form onSubmit={submitHandler}>
+         <input type='text' value={input} style={{"textTransform":"uppercase"}} onChange={(e) => setInput(e.target.value)} />
+         <button type='submit'>Submit</button>
+         {
+            // students && students.map(student => (
+            //    <div key={student.id}>
+            //       <p>ID: {student.ID}</p>
+            //    </div>
+            // ))
+         }
+         </form>
+      </div>
+   )
 }
-
 export default Homepage;

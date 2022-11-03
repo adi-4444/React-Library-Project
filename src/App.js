efbf766d9c484d8b99f43daddfc24ee3392446c2
Adi Narayana Reddy <adinarayana4444@gmail.com>
2022-11-03 14:03:16 +0530

    React Library App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Library from './components/Library';
 import { Routes, Route } from 'react-router-dom';
 import Students from './components/Students';
 import { BrowserRouter } from 'react-router-dom'
+import Notfound404 from './components/warning/Notfound404'
 function App() {
 
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Library />} />
           <Route path='/students' element={<Students />} />
+          <Route path='*' element={<Notfound404 />} />
         </Routes>
       </BrowserRouter>
 
